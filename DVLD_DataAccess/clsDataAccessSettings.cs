833e89ae2a9993517c2128240aedcd888ee09da0
zain <zainsleman97@gmail.com>
2026-05-14 07:15:07 -0700

    DataAccessLyer

---
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Configuration;
+
+namespace DVLD_DataAccess
+{
+    static class clsDataAccessSettings
+    {
+    //  public static string ConnectionString = "Server=.;Database=DVLD;User Id=sa;Password=sa123456;";
+
+       public static string ConnectionString { get { return ConfigurationManager.ConnectionStrings["connectionstring"].ConnectionString; } }
+
+    }
+}
---
+++ DVLD_DataAccess/clsEventLog.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccess
+{
+    public class clsEventLog:EventLog
+    {
+
+        public static string SourceName = "DVLD";
+
+        public enum enType { Application = 1, Security = 2, Setup = 3, System = 4, ForwardedEvent = 5 }
+
+        static string _SelectType(enType Type)
+        {
+            switch (Type)
+            {
+                case enType.Application:
+                    return "Applicaton";
+                case enType.Security:
+                    return "Security";
+                case enType.Setup:
+                    return "Setup";
+                case enType.System:
+                    return "System";
+                case enType.ForwardedEvent:
+                    return "Forwarded Event";
+                default:
+                    return "";
+            }
+        }
+
+
+        public static void EventLogException(Exception ex, enType Location, EventLogEntryType Type)
+        {
+
+            _SelectType(Location);
+
+            if (!Exists(SourceName))
+            {
+                CreateEventSource(SourceName, _SelectType(Location));
+            }
+
+            WriteEntry(SourceName, ex.ToString(), Type);
+
+
+
+        }
+
+        public static void WriteEvent(string EventLogName, Exception ex, enType Location, EventLogEntryType Type)
+        {
+
+            _SelectType(Location);
+
+            if (!SourceExists(SourceName))
+            {
+                CreateEventSource(SourceName, _SelectType(Location));
+            }
+
+            WriteEntry(SourceName, ex.ToString(), Type);
+
+
+
+        }
+
+
+
+    }
+}
